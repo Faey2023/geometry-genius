80f4fc90f1a3de04dda7d5188d0c92b7b7cc2fa6
Fahima <sfahima1388@gmail.com>
2023-08-20 10:38:44 +0600

    first commit

---
+++ area.js
@@ -0,0 +1,17 @@
+// triangle
+function calculateTriangleArea() {
+  //get triangle base value
+  const baseField = document.getElementById("triangle-base");
+  const baseValueText = baseField.value;
+  const base = parseFloat(baseValueText);
+  //get triangle height
+  const heightField = document.getElementById("triangle-height");
+  const heightValueText = heightField.value;
+  const height = parseFloat(heightValueText);
+}
+//rectangle
+function calculateRectangleArea() {}
+function calculateParallelogramArea() {}
+function calculateRhombusArea() {}
+function calculatePentagonArea() {}
+function calculateEllipseArea() {}
