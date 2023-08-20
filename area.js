6b32b12a637b12dfe4f9462de09548e0d50b5a76
Fahima <sfahima1388@gmail.com>
2023-08-20 11:32:25 +0600

    parallelogram area added

--- area.js
+++ area.js
@@ -8,10 +8,42 @@ function calculateTriangleArea() {
   const heightField = document.getElementById("triangle-height");
   const heightValueText = heightField.value;
   const height = parseFloat(heightValueText);
+  const areaT = 0.5 * base * height;
+  // show area
+  const areaSpanT = document.getElementById("triangle-area");
+  areaSpanT.innerText = areaT;
 }
 //rectangle
-function calculateRectangleArea() {}
-function calculateParallelogramArea() {}
+function calculateRectangleArea() {
+  const widthField = document.getElementById("rectangle-width");
+  const widthValueText = widthField.value;
+  const width = parseFloat(widthValueText);
+  //length
+  const lengthField = document.getElementById("rectangle-length");
+  const lengthValueText = lengthField.value;
+  const length = parseFloat(lengthValueText);
+  // area
+  const areaR = width * length;
+  // show area
+  const areaSpanR = document.getElementById("rectangle-area");
+  areaSpanR.innerText = areaR;
+}
+// Parallelogram
+function calculateParallelogramArea() {
+  const basePField = document.getElementById("parallelogram-base");
+  const basePvalueText = basePField.value;
+  const baseP = parseFloat(basePvalueText);
+  // height
+  const heightPField = document.getElementById("parallelogram-height");
+  const heightPValueText = heightPField.value;
+  const heightP = parseFloat(heightPValueText);
+  // area
+  const areaP = baseP * heightP;
+  // show area
+  const areaSpan = document.getElementById("parallelogram-area");
+  areaSpan.innerText = areaP;
+}
 function calculateRhombusArea() {}
+// pentagon
 function calculatePentagonArea() {}
 function calculateEllipseArea() {}
